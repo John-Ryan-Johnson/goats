082ac128445875d2dd45a718f761fd6a53be0d07
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-09 20:51:26 -0600

    prop-types

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import goatShape from '../../helpers/propz/goatShape';
 
 class Goat extends React.Component {
+  static propTypes = {
+    goat: goatShape.goatShape,
+  }
+
   render() {
     const { goat } = this.props;
     return (
@@ -10,7 +15,7 @@ class Goat extends React.Component {
             <h5 className="card-title">{goat.name}</h5>
              <p className="card-text">{goat.age}</p>
              <p className="card-text">{goat.description}</p>
-         </div>
+          </div>
       </div>
     );
   }
